96643c86abecbd1c4ee636633491b13051d0d18d
Java_programmers-STEPS <sreelakshmis_2445@spectrum.net.in>
2023-08-05 10:39:51 +0530

    05082023

--- lib/sqlexample/helper.dart
+++ lib/sqlexample/helper.dart
@@ -3,7 +3,7 @@ import 'package:sqflite/sqflite.dart' as sql;
 class SQLHelper {
   // Create table
 
-  Future<void> createTable(sql.Database database) async {
+  static Future<void> createTable(sql.Database database) async {
     await database.execute(""" CREATE TABLE records(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     title TEXT NOT NULL,
@@ -13,7 +13,7 @@ class SQLHelper {
   }
 
   //Create Database
-  Future<sql.Database> createDatabase() async {
+  static Future<sql.Database> createDatabase() async {
     return await sql.openDatabase(
       'spectrum.db',
       version: 1,
@@ -24,7 +24,24 @@ class SQLHelper {
   }
 
 // Create item
+  static Future<int> createItem(String? title, String? description) async {
+    final database = await SQLHelper.createDatabase();
+
+    final row = {'title': title, 'description': description};
+
+    final rowId = await database.insert('records', row,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+
+    return rowId;
+  }
+
 // Read table items
+
+  static Future<List<Map<String, dynamic>>> readAllItems() async {
+    final database = await SQLHelper.createDatabase();
+    return database.query('records', orderBy: 'id');
+  }
+
 // Update
 // Delete
 // Get all items
